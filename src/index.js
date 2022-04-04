5b286e6cdd7d9d7295421d04dc991b50b464c280
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-04 10:26:53 +0300

    Configure Auth0 provider for the entire application

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {Auth0Provider} from "@auth0/auth0-react"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Auth0Provider domain="dev-3ibqx6yh.us.auth0.com" clientId="0nPzfXA9vHRCuAMRsgrUzbfNjqpMUhnP" redirectUri={window.location.origin} >
     <App />
+  </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

a1180c8277c914db7d986904ff1aafeae3ada1af
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-05 07:03:13 +0300

    Use regular function instead of arrow functions in Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
 
-const LoginButton = () => {
+export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
 
   return <button onClick={() => loginWithRedirect()}>Log In</button>;
-};
-
-export default LoginButton;
+}

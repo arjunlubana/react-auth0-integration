f93f7ec35ac8ac9caa5b11a460926d9a26389a94
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-05 21:39:15 +0300

    Change color of login button to secondary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useAuth0 } from "@auth0/auth0-react";
-
 import {
   AppBar,
   Container,
@@ -13,7 +12,7 @@ import LoginButton from "./Login";
 import UserMenu from "./UserMenu";
 
 function Navbar() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
   return (
     <AppBar position="static">
@@ -39,15 +38,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            {isLoading ? (
-              <Stack sx={{ color: "grey.500" }} spacing={2} direction="row">
-                <CircularProgress color="secondary" />
-              </Stack>
-            ) : isAuthenticated ? (
-              <UserMenu />
-            ) : (
-              <LoginButton />
-            )}
+            {isAuthenticated ? <UserMenu /> : <LoginButton />}
           </Box>
         </Toolbar>
       </Container>

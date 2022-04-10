a4c55937fc70f11eb3420b3d4d1e6f55fa5a1ed5
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-10 09:09:34 +0300

    Update Profile page to work with useApi hook

--- src/views/EditProfile.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function EditProfile() {
-  return (
-    <div>EditProfile</div>
-  )
-}
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Typography, Box, Avatar, Skeleton } from "@mui/material";
+import useApi from "../hooks/useApi";
 
 export default function Profile() {
-  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
-    useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
+  const opts = {
+    audience: "https://dev-3ibqx6yh.us.auth0.com/api/v2/",
+    scope: "read:current_user",
+  };
+  const { user, loginWithRedirect, getAccessTokenWithPopup } = useAuth0();
 
-  useEffect(() => {
-    const getUserMetadata = async () => {
-      const domain = "dev-3ibqx6yh.us.auth0.com";
-
-      try {
-        const accessToken = await getAccessTokenSilently({
-          audience: `https://${domain}/api/v2/`,
-          scope: "read:current_user",
-        });
-
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const user_metadata = await metadataResponse.json();
-
-        setUserMetadata(user_metadata);
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
-
-    getUserMetadata();
-  }, [getAccessTokenSilently, user?.sub]);
-
-  if (isLoading) {
+  const {
+    loading,
+    error,
+    refresh,
+    data: userMetadata,
+  } = useApi(
+    `https://dev-3ibqx6yh.us.auth0.com/api/v2/users/${user?.sub}`,
+    opts
+  );
+  const getTokenAndTryAgain = async () => {
+    await getAccessTokenWithPopup(opts);
+    refresh();
+  };
+  if (loading) {
     return (
       <Box
         sx={{
@@ -58,33 +44,38 @@ export default function Profile() {
       </Box>
     );
   }
+  if (error) {
+    if (error.error === "login_required") {
+      return (
+        <button
+          onClick={() =>
+            loginWithRedirect(opts)
+          }
+        >
+          Login
+        </button>
+      );
+    }
+    if (error.error === "consent_required") {
+      return (
+        <button onClick={getTokenAndTryAgain}>Consent to reading users</button>
+      );
+    }
+    return <div>Oops {error.message}</div>;
+  }
 
   return (
-    isAuthenticated && (
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          maxWidth: "sm",
-          mx: "auto",
-        }}
-      >
-        <Avatar
-          src={user.picture}
-          alt={user.name}
-          sx={{ width: "128px", height: "128px" }}
-        />
-        <Typography variant="h3">{user.name}</Typography>
-        <Typography variant="h5">{user.email}</Typography>
-        <Typography variant="h3">User Metadata</Typography>
-        {userMetadata ? (
-          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-        ) : (
-          "No user metadata defined"
-        )}
-      </Box>
-    )
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        maxWidth: "sm",
+        mx: "auto",
+      }}
+    >
+      {JSON.stringify(userMetadata, null, 2)}
+    </Box>
   );
 }

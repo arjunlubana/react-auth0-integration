d829c51fc1b35f078408f7d76fae989ce3610d72
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-12 18:13:38 +0300

    Add routes to navigate to the profile page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Content from "./views/Content";
-import Profile from "./views/Profile";
+import ProfileOverview from "./views/ProfileOverview";
+import ProfileSettings from "./views/ProfileSettings";
 
 export default function App() {
   return (
-      <div>
-        <Navbar />
-        <Routes path="/">
-          <Route index element={<Content />} />
-          <Route path="profile" element={<Profile />} />
-        </Routes>
-      </div>
+    <div>
+      <Navbar />
+      <Routes path="/">
+        <Route index element={<Content />} />
+        <Route path="profile" element={<ProfileOverview />}>
+          <Route path="personal" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }

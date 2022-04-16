fc52aaf6258113643b64d116942f287c9a2cd75a
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-16 08:39:42 +0300

    Integrate react hook form

--- src/views/ProfileOverview.jsx
+++ src/views/ProfileOverview.jsx
@@ -1,29 +1,45 @@
-import { Button, TextField } from "@mui/material";
+import { Container, Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 export default function ProfileOverview({ userData }) {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => alert(JSON.stringify(data));
 	return (
-		<div>
-			<form>
+		<Container>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<TextField
 					id="first_name"
 					label="First Name"
 					variant="outlined"
 					defaultValue={userData.given_name}
+					{...register("first_name")}
+					sx={{ m: "1rem" }}
 				/>
 				<TextField
 					id="last_name"
 					label="Last Name"
 					variant="outlined"
 					defaultValue={userData.family_name}
+					{...register("last_name")}
+					sx={{ m: "1rem" }}
 				/>
 				<TextField
 					id="email"
 					label="E-mail"
 					variant="outlined"
 					defaultValue={userData.email}
+					{...register("email")}
+					sx={{ m: "1rem" }}
 				/>
-
-				<Button type="submit" variant="primary">Save Changes</Button>
+				<br />
+				<Button type="submit" variant="outlined" sx={{ m: "1rem" }}>
+					Save Changes
+				</Button>
 			</form>
 			<p>Joined in {userData.created_at}</p>
 			<div>
@@ -32,6 +48,6 @@ export default function ProfileOverview({ userData }) {
 					<p>{identity.provider}</p>
 				))}
 			</div>
-		</div>
+		</Container>
 	);
 }

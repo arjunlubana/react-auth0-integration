583825346d10120ce1838e13ccc1f6d1b8bc9711
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-17 05:45:44 +0300

    Move data fetching from the API to ProfileOverview from Parent Profile

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,65 +1,32 @@
 import { Fragment } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Container } from "@mui/material";
-import { useApi } from "hooks";
 import { ProfileOverview, ProfileSettings } from "views";
+import { TabsHeader, TabsPanel, ProfileInfo } from "components";
 import { ProfileLoader } from "loaders";
-import { TabsHeader, TabsPanel, ProfileInfo, LoginButton } from "components";
 
 export default function Profile() {
-	const { user, getAccessTokenWithPopup } = useAuth0();
-	const tokenOptions = {
-		audience: process.env.REACT_APP_AUTH0_MANAGEMENT_API,
-		scope: "read:current_user",
-	};
+	const { user, isLoading } = useAuth0();
 
-	const fetchOptions = {
-		headers: {},
-	};
-	const { error, loading, data, refresh } = useApi(
-		`${process.env.REACT_APP_AUTH0_MANAGEMENT_API}users/${user?.sub}`,
-		tokenOptions,
-		fetchOptions
-	);
-
-	const getTokenAndTryAgain = async () => {
-		await getAccessTokenWithPopup(tokenOptions);
-		refresh();
-	};
-
-	if (loading) {
+	if (isLoading) {
 		return <ProfileLoader />;
 	}
-	if (error) {
-		if (error.error === "login_required") {
-			return <LoginButton />;
-		}
-		if (error.error === "consent_required") {
-			return (
-				<Button onClick={getTokenAndTryAgain}>
-					Consent to reading user info
-				</Button>
-			);
-		}
-		return <div>Oops {error.name}</div>;
-	}
+
 	return (
-		user && (
-			<Container>
-				<ProfileInfo name={user.name} image={user.picture} />
-				<TabsHeader>
-					{(value) => (
-						<Fragment>
-							<TabsPanel value={value} index={0}>
-								<ProfileOverview userData={data} />
-							</TabsPanel>
-							<TabsPanel value={value} index={1}>
-								<ProfileSettings userData={data} />
-							</TabsPanel>
-						</Fragment>
-					)}
-				</TabsHeader>
-			</Container>
-		)
+		<Container>
+			<ProfileInfo name={user.name} image={user.picture} />
+			<TabsHeader>
+				{(value) => (
+					<Fragment>
+						<TabsPanel value={value} index={0}>
+							<ProfileOverview />
+						</TabsPanel>
+						<TabsPanel value={value} index={1}>
+							<ProfileSettings />
+						</TabsPanel>
+					</Fragment>
+				)}
+			</TabsHeader>
+		</Container>
 	);
 }
--- src/views/ProfileOverview.jsx
+++ src/views/ProfileOverview.jsx
@@ -1,70 +1,60 @@
-import { Container, Button, TextField } from "@mui/material";
-import { useForm } from "react-hook-form";
-import { DateTime } from "luxon";
-import { Auth0Icon, TwitterIcon, GoogleIcon } from "assets";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useApi } from "hooks";
+import { ProfileLoader } from "loaders";
+import { Container, Button, Typography } from "@mui/material";
+import { DateTime } from "luxon";
+import { LoginButton } from "components";
+import { Auth0Icon, TwitterIcon, GoogleIcon } from "assets";
 
-export default function ProfileOverview({ userData }) {
-	const dt = DateTime.fromISO(userData.created_at);
+export default function ProfileOverview() {
+	const { user, getAccessTokenWithPopup } = useAuth0();
+	const tokenOptions = {
+		audience: process.env.REACT_APP_AUTH0_MANAGEMENT_API,
+		scope: "read:current_user",
+	};
 
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
-	const onSubmit = (data) => alert(JSON.stringify(data));
+	const fetchOptions = {
+		headers: {},
+	};
+	const { error, loading, data, refresh } = useApi(
+		`${process.env.REACT_APP_AUTH0_MANAGEMENT_API}users/${user?.sub}`,
+		tokenOptions,
+		fetchOptions
+	);
+
+	const getTokenAndTryAgain = async () => {
+		await getAccessTokenWithPopup(tokenOptions);
+		refresh();
+	};
+
+	if (loading) {
+		return <ProfileLoader />;
+	}
+	if (error) {
+		if (error.error === "login_required") {
+			return <LoginButton />;
+		}
+		if (error.error === "consent_required") {
+			return (
+				<Button onClick={getTokenAndTryAgain}>
+					Consent to reading user info
+				</Button>
+			);
+		}
+		return <div>Oops {error.name}</div>;
+	}
 
 	return (
-		<Container
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<TextField
-					id="given_name"
-					label="First Name"
-					variant="outlined"
-					defaultValue={userData.given_name}
-					{...register("given_name")}
-					sx={{ m: "1rem" }}
-				/>
-				<TextField
-					id="family_name"
-					label="Last Name"
-					variant="outlined"
-					defaultValue={userData.family_name}
-					{...register("family_name")}
-					sx={{ m: "1rem" }}
-				/>
-				<TextField
-					id="email"
-					label="E-mail"
-					variant="outlined"
-					defaultValue={userData.email}
-					{...register("email")}
-					sx={{ m: "1rem" }}
-				/>
-				<br />
-				<Button
-					type="submit"
-					variant="outlined"
-					sx={{
-						m: "1rem",
-					}}
-				>
-					Save Changes
-				</Button>
-			</form>
-
+		data && (
 			<Container sx={{ textAlign: "center" }}>
-				<p>Joined in {dt.toLocaleString(DateTime.DATETIME_FULL)}</p>
+				<p>
+					Joined in
+					{DateTime.fromISO(data.created_at).toLocaleString(
+						DateTime.DATETIME_FULL
+					)}
+				</p>
 				<p>Connected Account</p>
-				{userData.identities.map((identity) => {
+				{data.identities.map((identity) => {
 					if (identity.provider === "google-oauth2") {
 						return <img src={GoogleIcon} alt="google" />;
 					} else if (identity.provider === "twitter") {
@@ -74,6 +64,6 @@ export default function ProfileOverview({ userData }) {
 					}
 				})}
 			</Container>
-		</Container>
+		)
 	);
 }
